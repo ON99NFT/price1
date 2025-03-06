baf1054c7f03057783b65e1347ec332c9ee45509
ON99NFT <128242803+ON99NFT@users.noreply.github.com>
2025-03-05 19:15:19 -0500

    hood.js

--- hood.js
+++ hood.js
@@ -249,13 +249,13 @@ const hood = (() => {
       );
       
       let playSound = false;
-      if (difference > 0.0000012) {
+      if (difference > 0.0000018) {
           element.classList.add('alert-flashing-2');
           playSound = true;
-      } else if (difference > 0.0000008) {
+      } else if (difference > 0.0000012) {
           element.classList.add('alert-flashing-1');
           playSound = true;
-      } else if (difference > 0.0000004) {
+      } else if (difference > 0.0000006) {
           element.classList.add('alert-large-green');
       } else if (difference > 0) {
           element.classList.add('alert-positive');
