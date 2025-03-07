d7e5cad7507d721a5be59a14e214f50293ea1891
ON99NFT <128242803+ON99NFT@users.noreply.github.com>
2025-03-07 13:43:50 -0500

    Update hood.js

--- hood.js
+++ hood.js
@@ -249,13 +249,13 @@ const hood = (() => {
       );
       
       let playSound = false;
-      if (difference > 0.0000012) {
+      if (difference > 0.000002) {
           element.classList.add('alert-flashing-2');
           playSound = true;
-      } else if (difference > 0.0000008) {
+      } else if (difference > 0.000001) {
           element.classList.add('alert-flashing-1');
           playSound = true;
-      } else if (difference > 0.0000004) {
+      } else if (difference > 0.0000005) {
           element.classList.add('alert-large-green');
       } else if (difference > 0) {
           element.classList.add('alert-positive');
