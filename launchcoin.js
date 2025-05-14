043fcd3b356b2238cba93c2c41c4354a7ae16f25
ON99NFT <128242803+ON99NFT@users.noreply.github.com>
2025-05-13 20:39:14 -0400

    Update launchcoin.js

--- launchcoin.js
+++ launchcoin.js
@@ -176,13 +176,13 @@ const launchcoin = (() => {
         element.style.fontSize = '';
         
         let playSound = false;
-        if (difference > 0.01) {
+        if (difference > 0.006) {
             element.classList.add('alert-flashing-2');
             playSound = true;
-        } else if (difference > 0.006) {
+        } else if (difference > 0.004) {
             element.classList.add('alert-flashing-1');
             playSound = true;
-        } else if (difference > 0.003) {
+        } else if (difference > 0.002) {
             element.classList.add('alert-large-green');
         } else if (difference > 0) {
             element.classList.add('alert-positive');
